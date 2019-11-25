5b0f417e29b120fdd46053b27dfd3054f31ae518
Shaun Mathias <smathias@companieshouse.gov.uk>
2019-11-25 13:30:56 +0000

    Initiall creation of Makefile, start.sh and pom.xml for search.api Added version file for concourse Added .gitignore

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Start script for search.api.ch.gov.uk
+
+APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ -z "${MESOS_SLAVE_PID}" ]]; then
+    source ~/.chs_env/private_env
+    source ~/.chs_env/global_env
+    source ~/.chs_env/search.api.ch.gov.uk/env
+
+    PORT="${SEARCH_API_SERVICE_PORT}"
+else
+    PORT="$1"
+    CONFIG_URL="$2"
+    ENVIRONMENT="$3"
+    APP_NAME="$4"
+
+    echo "Downloading environment from: ${CONFIG_URL}/${ENVIRONMENT}/${APP_NAME}"
+    wget -O "${APP_DIR}/private_env" "${CONFIG_URL}/${ENVIRONMENT}/private_env"
+    wget -O "${APP_DIR}/global_env" "${CONFIG_URL}/${ENVIRONMENT}/global_env"
+    wget -O "${APP_DIR}/app_env" "${CONFIG_URL}/${ENVIRONMENT}/${APP_NAME}/env"
+    source "${APP_DIR}/private_env"
+    source "${APP_DIR}/global_env"
+    source "${APP_DIR}/app_env"
+fi
+
+exec java ${JAVA_MEM_ARGS} -jar -Dserver.port="${PORT}" -Dlog4j.configurationFile="${APP_DIR}/log4j2.xml" "${APP_DIR}/search.api.ch.gov.uk.jar"
