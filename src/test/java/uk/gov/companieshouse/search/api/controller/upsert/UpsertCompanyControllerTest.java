890f26f3002cbcf9b3b9a4c466a15f8742f87322
caismith-ch <csmith2@companieshouse.gov.uk>
2019-12-05 17:25:43 +0000

    Increase coverage of controller tests

--- src/test/java/uk/gov/companieshouse/search/api/controller/upsert/UpsertCompanyControllerTest.java
+++ src/test/java/uk/gov/companieshouse/search/api/controller/upsert/UpsertCompanyControllerTest.java
@@ -18,8 +18,11 @@ import uk.gov.companieshouse.search.api.service.upsert.UpsertCompanyService;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.OK;
 import static uk.gov.companieshouse.search.api.model.response.ResponseStatus.DOCUMENT_UPSERTED;
+import static uk.gov.companieshouse.search.api.model.response.ResponseStatus.UPDATE_REQUEST_ERROR;
+import static uk.gov.companieshouse.search.api.model.response.ResponseStatus.UPSERT_ERROR;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -35,7 +38,7 @@ public class UpsertCompanyControllerTest {
     private UpsertCompanyController upsertCompanyController;
 
     @Test
-    @DisplayName("")
+    @DisplayName("Test upsert company is successful")
     void testUpsertSuccessful() {
         ResponseObject responseObject = new ResponseObject(DOCUMENT_UPSERTED);
         Company company = createCompany();
@@ -48,6 +51,38 @@ public class UpsertCompanyControllerTest {
 
         assertNotNull(responseEntity);
         assertEquals(OK, responseEntity.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Test upsert company failed to index or update document")
+    void testUpsertFailedToIndexOrUpdate() {
+        ResponseObject responseObject = new ResponseObject(UPSERT_ERROR);
+        Company company = createCompany();
+
+        when(mockUpsertCompanyService.upsert(company)).thenReturn(responseObject);
+        when(mockApiToResponseMapper.map(responseObject))
+            .thenReturn(ResponseEntity.status(INTERNAL_SERVER_ERROR).build());
+
+        ResponseEntity responseEntity = upsertCompanyController.upsertCompany(company);
+
+        assertNotNull(responseEntity);
+        assertEquals(INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Test upsert failed during update request")
+    void testUpsertFailedUpdateRequest() {
+        ResponseObject responseObject = new ResponseObject(UPDATE_REQUEST_ERROR);
+        Company company = createCompany();
+
+        when(mockUpsertCompanyService.upsert(company)).thenReturn(responseObject);
+        when(mockApiToResponseMapper.map(responseObject))
+            .thenReturn(ResponseEntity.status(INTERNAL_SERVER_ERROR).build());
+
+        ResponseEntity responseEntity = upsertCompanyController.upsertCompany(company);
+
+        assertNotNull(responseEntity);
+        assertEquals(INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
     }
 
     private Company createCompany() {
