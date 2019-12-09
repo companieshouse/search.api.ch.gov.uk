3f1e603541fc7a733a586915502ae1323684f897
caismith-ch <csmith2@companieshouse.gov.uk>
2019-12-09 13:03:51 +0000

    Add tests for exceptions in UpsertCompanyService and increase class coverage to 100%

--- src/test/java/uk/gov/companieshouse/search/api/service/upsert/UpsertCompanyServiceTest.java
+++ src/test/java/uk/gov/companieshouse/search/api/service/upsert/UpsertCompanyServiceTest.java
@@ -9,17 +9,23 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.search.api.exception.IndexException;
+import uk.gov.companieshouse.search.api.exception.UpsertException;
 import uk.gov.companieshouse.search.api.model.esdatamodel.company.Company;
 import uk.gov.companieshouse.search.api.model.esdatamodel.company.Items;
 import uk.gov.companieshouse.search.api.model.esdatamodel.company.Links;
 import uk.gov.companieshouse.search.api.model.response.ResponseObject;
 import uk.gov.companieshouse.search.api.service.rest.RestClientService;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static uk.gov.companieshouse.search.api.model.response.ResponseStatus.DOCUMENT_UPSERTED;
+import static uk.gov.companieshouse.search.api.model.response.ResponseStatus.UPDATE_REQUEST_ERROR;
+import static uk.gov.companieshouse.search.api.model.response.ResponseStatus.UPSERT_ERROR;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -39,7 +45,7 @@ public class UpsertCompanyServiceTest {
     void testUpsertIsSuccessful() throws Exception {
 
         Company company = createCompany();
-        IndexRequest indexRequest = new IndexRequest();
+        IndexRequest indexRequest = new IndexRequest("alpha_search");
 
         when(mockUpsertRequestService.createIndexRequest(company)).thenReturn(indexRequest);
         when(mockUpsertRequestService.createUpdateRequest(
@@ -49,6 +55,58 @@ public class UpsertCompanyServiceTest {
 
         assertNotNull(responseObject);
         assertEquals(DOCUMENT_UPSERTED, responseObject.getStatus());
+    }
+
+    @Test
+    @DisplayName("Test exception thrown during index request")
+    void testExceptionThrownDuringIndexRequest() throws Exception {
+
+        Company company = createCompany();
+        IndexRequest indexRequest = new IndexRequest("alpha_search");
+
+        when(mockUpsertRequestService.createIndexRequest(company)).thenThrow(IndexException.class);
+
+        ResponseObject responseObject = upsertCompanyService.upsert(company);
+
+        assertNotNull(responseObject);
+        assertEquals(UPSERT_ERROR, responseObject.getStatus());
+    }
+
+    @Test
+    @DisplayName("Test exception thrown during update request")
+    void testExceptionThrownDuringUpdateRequest() throws Exception {
+
+        Company company = createCompany();
+        IndexRequest indexRequest = new IndexRequest("alpha_search");
+
+        when(mockUpsertRequestService.createIndexRequest(company)).thenReturn(indexRequest);
+        when(mockUpsertRequestService.createUpdateRequest(
+            company, indexRequest)).thenThrow(UpsertException.class);
+
+        ResponseObject responseObject = upsertCompanyService.upsert(company);
+
+        assertNotNull(responseObject);
+        assertEquals(UPSERT_ERROR, responseObject.getStatus());
+    }
+
+    @Test
+    @DisplayName("Test exception thrown during upsert")
+    void testExceptionThrownDuringUpsert() throws Exception {
+
+        Company company = createCompany();
+        IndexRequest indexRequest = new IndexRequest("alpha_search");
+        UpdateRequest updateRequest = new UpdateRequest("alpha_search", company.getId());
+
+        when(mockUpsertRequestService.createIndexRequest(company)).thenReturn(indexRequest);
+        when(mockUpsertRequestService.createUpdateRequest(
+            company, indexRequest)).thenReturn(updateRequest);
+
+        when(mockRestClientService.upsert(updateRequest)).thenThrow(IOException.class);
+
+        ResponseObject responseObject = upsertCompanyService.upsert(company);
+
+        assertNotNull(responseObject);
+        assertEquals(UPDATE_REQUEST_ERROR, responseObject.getStatus());
     }
 
     private Company createCompany() {
