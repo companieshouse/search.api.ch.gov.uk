30b5547186246cd477c179948f679e38fed64414
caismith-ch <csmith2@companieshouse.gov.uk>
2020-03-13 08:03:53 +0000

    Add missing bean for rest template

--- src/main/java/uk/gov/companieshouse/search/api/SearchApiApplicationConfiguration.java
+++ src/main/java/uk/gov/companieshouse/search/api/SearchApiApplicationConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.companieshouse.search.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
 
@@ -11,5 +12,10 @@ public class SearchApiApplicationConfiguration {
     @Bean
     EnvironmentReader environmentReader() {
         return new EnvironmentReaderImpl();
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
