00a30086a104de526e22a9e5e0c5bc1a37990b14
caismith-ch <csmith2@companieshouse.gov.uk>
2021-02-09 17:52:34 +0000

    Remove service impl file in favour of singular service and add javadoc

--- src/main/java/uk/gov/companieshouse/search/api/service/rest/RestClientService.java
+++ src/main/java/uk/gov/companieshouse/search/api/service/rest/RestClientService.java
@@ -1,31 +1,45 @@
-package uk.gov.companieshouse.search.api.service.rest;
+package uk.gov.companieshouse.search.api.service.rest.impl;
+
+import static org.elasticsearch.client.RequestOptions.DEFAULT;
+
+import java.io.IOException;
 
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
-import java.io.IOException;
+import uk.gov.companieshouse.search.api.service.rest.RestClientService;
 
-public interface RestClientService {
+@Service
+public class RestClientServiceImpl implements RestClientService {
 
-    /**
-     * interface for elastic search high level rest client used for search
-     *
-     * @param searchRequest - searchRequest containing search parameters
-     * @return SearchResponse - response from elastic search db
-     */
-    SearchResponse searchAlphabeticalRestClient(SearchRequest searchRequest) throws IOException;
-
-    /**
-     * interface for elastic search high level rest client used in upsert
-     *
-     * @param updateRequest - updateRequest containing update parameters
-     * @return UpdateResponse - response from elastic search db
-     */
-    UpdateResponse upsertAlphabeticalRestClient(UpdateRequest updateRequest) throws IOException;
+    @Autowired
+    @Qualifier("alphabeticalClient")
+    private RestHighLevelClient alphabeticalClient;
     
-    SearchResponse searchDissolvedRestClient(SearchRequest searchRequest) throws IOException;
-    
-    
+    @Autowired
+    @Qualifier("dissolvedClient")
+    private RestHighLevelClient dissolvedClient;
+
+    @Override
+    public SearchResponse searchAlphabeticalRestClient(SearchRequest searchRequest) throws IOException {
+        return alphabeticalClient.search(searchRequest, DEFAULT);
+    }
+
+    @Override
+    public UpdateResponse upsertAlphabeticalRestClient(UpdateRequest updateRequest) throws IOException {
+        return alphabeticalClient.update(updateRequest, RequestOptions.DEFAULT);
+    }
+
+	@Override
+	public SearchResponse searchDissolvedRestClient(SearchRequest searchRequest) throws IOException {
+		// TODO Auto-generated method stub
+		return dissolvedClient.search(searchRequest, DEFAULT);
+	}
 }
