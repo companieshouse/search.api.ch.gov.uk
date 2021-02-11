0d1ae7e336070b9523e7b854ef73b453c4efde34
caismith-ch <csmith2@companieshouse.gov.uk>
2021-02-11 16:57:27 +0000

    Add index service with dummy data, basic unit test and refactor alpha search test files

--- src/main/java/uk/gov/companieshouse/search/api/service/search/impl/dissolved/DissolvedSearchIndexService.java
+++ src/main/java/uk/gov/companieshouse/search/api/service/search/impl/dissolved/DissolvedSearchIndexService.java
@@ -5,8 +5,14 @@ import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 import uk.gov.companieshouse.search.api.model.DissolvedSearchResults;
 import uk.gov.companieshouse.search.api.model.TopHit;
+import uk.gov.companieshouse.search.api.model.esdatamodel.dissolved.Address;
+import uk.gov.companieshouse.search.api.model.esdatamodel.dissolved.DissolvedCompany;
+import uk.gov.companieshouse.search.api.model.esdatamodel.dissolved.PreviousCompanyName;
 import uk.gov.companieshouse.search.api.model.response.DissolvedResponseObject;
 import uk.gov.companieshouse.search.api.model.response.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static uk.gov.companieshouse.search.api.SearchApiApplication.APPLICATION_NAME_SPACE;
 
@@ -19,13 +25,38 @@ public class DissolvedSearchIndexService {
 
     public DissolvedResponseObject search(String companyName, String requestId) {
 
-        DissolvedSearchResults searchResults = new DissolvedSearchResults();
-        searchResults.setEtag("TEST");
         TopHit topHit = new TopHit();
         topHit.setCompanyName("TEST COMPANY NAME");
         topHit.setCompanyNumber("TEST COMPANY NUMBER");
+
+        List<DissolvedCompany> dissolvedCompanies = new ArrayList<>();
+        DissolvedCompany dissolvedCompany = new DissolvedCompany();
+        Address address = new Address();
+        address.setLocality("TEST LOCALITY");
+        address.setPostalCode("AB12 3CD");
+
+        PreviousCompanyName previousCompanyName = new PreviousCompanyName();
+        previousCompanyName.setDateOfNameCessation("01/01/1993");
+        previousCompanyName.setDateOfNameEffectiveness("01/01/1983");
+        previousCompanyName.setName("PREVIOUS NAME");
+
+        List<PreviousCompanyName> previousCompanyNames = new ArrayList<>();
+        previousCompanyNames.add(previousCompanyName);
+
+        dissolvedCompany.setCompanyName("TEST COMPANY NAME");
+        dissolvedCompany.setCompanyNumber("TEST COMPANY NUMBER");
+        dissolvedCompany.setCompanyStatus("TEST COMPANY STATUS");
+        dissolvedCompany.setDateOfCessation("TEST DATE OF CESSATION");
+        dissolvedCompany.setDateOfCreation("TEST DATE OF CREATION");
+        dissolvedCompany.setAddress(address);
+        dissolvedCompany.setPreviousCompanyNames(previousCompanyNames);
+
+        dissolvedCompanies.add(dissolvedCompany);
+
+        DissolvedSearchResults searchResults = new DissolvedSearchResults();
         searchResults.setTopHit(topHit);
-        searchResults.setItems(null);
+        searchResults.setEtag("TEST");
+        searchResults.setItems(dissolvedCompanies);
 
         LOG.info(DISSOLVED_SEARCH + "successful for: " + companyName);
         return new DissolvedResponseObject(ResponseStatus.SEARCH_FOUND, searchResults);
