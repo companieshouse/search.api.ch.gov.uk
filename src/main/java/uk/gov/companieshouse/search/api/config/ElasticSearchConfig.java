85436eb0a21e7f214b09cc37e651838c2c70f49a
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2021-03-04 08:00:52 +0000

    Amended to enable individual configuration of VPC URL for dissolved search

--- src/main/java/uk/gov/companieshouse/search/api/config/ElasticSearchConfig.java
+++ src/main/java/uk/gov/companieshouse/search/api/config/ElasticSearchConfig.java
@@ -22,7 +22,7 @@ public class ElasticSearchConfig {
 
     // These are currently pointing at the existing ES instance, will need to be updated in the configs for both
     private static final String ALPHABETICAL_SEARCH_URL = "ELASTIC_SEARCH_URL";
-    private static final String DISSOLVED_SEARCH_URL = "ELASTIC_SEARCH_URL";
+    private static final String DISSOLVED_SEARCH_URL = "DISSOLVED_SEARCH_URL";
 
     @Qualifier("alphabeticalClient")
     @Bean(destroyMethod = "close")
