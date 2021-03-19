fee79f12b8d62c057c8b82e34a1a5bf87411090d
caismith-ch <csmith2@companieshouse.gov.uk>
2021-03-19 14:22:54 +0000

    Fix compilation error

--- src/main/java/uk/gov/companieshouse/search/api/elasticsearch/DissolvedSearchQueries.java
+++ src/main/java/uk/gov/companieshouse/search/api/elasticsearch/DissolvedSearchQueries.java
@@ -3,9 +3,6 @@ package uk.gov.companieshouse.search.api.elasticsearch;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.stereotype.Component;
-import uk.gov.companieshouse.search.api.util.StringTokeniserUtil;
-
-import java.util.List;
 
 @Component
 public class DissolvedSearchQueries extends AbstractSearchQuery {
