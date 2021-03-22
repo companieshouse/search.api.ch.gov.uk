2941856f397b19acddbc1c20d432ead4df4ca11c
caismith-ch <csmith2@companieshouse.gov.uk>
2021-03-22 09:28:01 +0000

    Fix typo in constant

--- src/main/java/uk/gov/companieshouse/search/api/logging/LoggingUtils.java
+++ src/main/java/uk/gov/companieshouse/search/api/logging/LoggingUtils.java
@@ -16,7 +16,7 @@ public class LoggingUtils {
     public static final String COMPANY_NAME = "company_name";
     public static final String COMPANY_NUMBER = "company_number";
     public static final String DISSOLVED_SEARCH_ALPHABETICAL = "dissolved - alphabetical";
-    public static final String DISSOLVED_SEARCH_BEST_MATCH = "dissolved - alphabetical";
+    public static final String DISSOLVED_SEARCH_BEST_MATCH = "dissolved - best match";
     public static final String INDEX = "index_name";
     public static final String INDEX_ALPHABETICAL = "alphabetical_search_index";
     public static final String INDEX_DISSOLVED = "dissolved_search_index";
