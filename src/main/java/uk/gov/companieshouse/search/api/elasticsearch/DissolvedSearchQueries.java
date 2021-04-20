c5e0e9a5e44a882ce77c3c5db5e20b851d933b56
caismith-ch <csmith2@companieshouse.gov.uk>
2021-04-20 16:44:50 +0100

    Use short name instead of full company name to improve results

--- src/main/java/uk/gov/companieshouse/search/api/elasticsearch/DissolvedSearchQueries.java
+++ src/main/java/uk/gov/companieshouse/search/api/elasticsearch/DissolvedSearchQueries.java
@@ -22,7 +22,7 @@ public class DissolvedSearchQueries extends AbstractSearchQuery {
     }
 
     public QueryBuilder createBestMatchQuery(String companyName) {
-        return QueryBuilders.matchQuery("company_name.doconly", companyName);
+        return QueryBuilders.matchQuery("short_name.company_name_best_match", companyName);
     }
 
     public QueryBuilder createPreviousNamesBestMatchQuery(String companyName) {
