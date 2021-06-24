b9f9ddf7df6c0032c5d791d2f4970147077d3680
ethatcher83 <ethatcher@companieshouse.gov.uk>
2021-06-24 10:04:33 +0100

    change to not include empty fields rather than null for search results model

--- src/main/java/uk/gov/companieshouse/search/api/model/SearchResults.java
+++ src/main/java/uk/gov/companieshouse/search/api/model/SearchResults.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class SearchResults<T> {
 
     @JsonProperty("etag")
