338aa4aceebe2c92769a8902e5d15a1eecf102fe
ethatcher83 <ethatcher@companieshouse.gov.uk>
2021-06-28 10:00:38 +0100

    change get to put in intercepter

--- src/main/java/uk/gov/companieshouse/search/api/interceptor/UserAuthorisationInterceptor.java
+++ src/main/java/uk/gov/companieshouse/search/api/interceptor/UserAuthorisationInterceptor.java
@@ -1,6 +1,6 @@
 package uk.gov.companieshouse.search.api.interceptor;
 
-import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.PUT;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static uk.gov.companieshouse.search.api.logging.LoggingUtils.REQUEST_ID_HEADER_NAME;
 import static uk.gov.companieshouse.search.api.logging.LoggingUtils.REQUEST_ID_LOG_KEY;
@@ -34,7 +34,7 @@ public class UserAuthorisationInterceptor extends HandlerInterceptorAdapter {
     private boolean validateAPI(HttpServletRequest request, HttpServletResponse response){
         Map<String, Object> logMap = new HashMap<>();
         logMap.put(REQUEST_ID_LOG_KEY, request.getHeader(REQUEST_ID_HEADER_NAME));
-        if(AuthorisationUtil.hasInternalUserRole(request) && GET.matches(request.getMethod())) {
+        if(AuthorisationUtil.hasInternalUserRole(request) && PUT.matches(request.getMethod())) {
             getLogger().info("internal API is permitted to view the resource", logMap);
             return true;
         } else {
