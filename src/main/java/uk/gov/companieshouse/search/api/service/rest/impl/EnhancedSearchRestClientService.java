55981d30949dea7daab0f032232ec742584c2d5f
caismith-ch <csmith2@companieshouse.gov.uk>
2021-07-12 15:28:46 +0100

    Add enhanced search rest high level client config and service

---
+++ src/main/java/uk/gov/companieshouse/search/api/service/rest/impl/EnhancedSearchRestClientService.java
@@ -0,0 +1,32 @@
+package uk.gov.companieshouse.search.api.service.rest.impl;
+
+import static org.elasticsearch.client.RequestOptions.DEFAULT;
+
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import uk.gov.companieshouse.search.api.service.rest.RestClientService;
+
+import java.io.IOException;
+
+public class EnhancedSearchRestClientService implements RestClientService {
+
+    @Autowired
+    @Qualifier("enhancedClient")
+    private RestHighLevelClient enhancedClient;
+
+    @Override
+    public SearchResponse search(SearchRequest searchRequest) throws IOException {
+        return enhancedClient.search(searchRequest, DEFAULT);
+    }
+
+    @Override
+    public UpdateResponse upsert(UpdateRequest updateRequest) throws IOException {
+        return enhancedClient.update(updateRequest, RequestOptions.DEFAULT);
+    }
+}
