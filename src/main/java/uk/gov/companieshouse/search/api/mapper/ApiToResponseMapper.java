ce1b940d431c19374451286082da9c91a3dd1e4e
ethatcher83 <ethatcher@companieshouse.gov.uk>
2022-03-11 12:26:55 +0000

    update error message when an incorrect company subtype value is provided update test

--- src/main/java/uk/gov/companieshouse/search/api/mapper/ApiToResponseMapper.java
+++ src/main/java/uk/gov/companieshouse/search/api/mapper/ApiToResponseMapper.java
@@ -39,7 +39,7 @@ public class ApiToResponseMapper {
             case MAPPING_ERROR:
                 return ResponseEntity.status(BAD_REQUEST)
                     .body("Error attempting to map request parameter values, please check the values of fields " +
-                        "'company_status' or 'company_type' contain accurate values");
+                        "'company_status' or 'company_type' or 'company_subtype' contain accurate values");
             case REQUEST_PARAMETER_ERROR:
                 return ResponseEntity.status(INTERNAL_SERVER_ERROR)
                         .body("Invalid url parameter for search_type, " +
