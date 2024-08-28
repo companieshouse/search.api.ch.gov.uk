1f50dcd9d229040063b892b2ad63d5c42334df93
NeilShadrach <neil.shadrach@corryn.com>
2024-08-28 09:50:59 +0100

    Change - to .

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Start script for search-api-ch-gov-uk
+# Start script for search.api.ch.gov.uk
 
 PORT=8080
 
-exec java -jar -Dserver.port="${PORT}" "search-api-ch-gov-uk.jar"
+exec java -jar -Dserver.port="${PORT}" "search.api.ch.gov.uk.jar"
