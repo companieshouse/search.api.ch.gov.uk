fb84679b7fa842d5b2d699d8f3961aae01b410a6
tombradley1 <tbradley@companieshouse.gov.uk>
2024-11-08 08:50:20 +0000

    add empty ROA converter test

--- src/test/java/uk/gov/companieshouse/search/api/mapper/CompanySearchItemConverterTest.java
+++ src/test/java/uk/gov/companieshouse/search/api/mapper/CompanySearchItemConverterTest.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.search.api.mapper;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -146,6 +147,45 @@ class CompanySearchItemConverterTest {
 
         //then
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void convertWithNullRoa() {
+        // given
+        CompanySearchItemConverterModel model = new CompanySearchItemConverterModel()
+                .partialData(false)
+                .companyName("TEST COMPANY PLC")
+                .dateOfCreation(LocalDate.of(2000, 6, 24))
+                .registeredOfficeAddress(null)
+                .companyNumber("ABCD1234")
+                .externalRegistrationNumber("CDEF3456")
+                .dateOfCessation(LocalDate.of(2024, 6, 24))
+                .sicCodes(Arrays.asList("12345", "23456", "34567"))
+                .companyStatus("active")
+                .alphaKey("TESTCOMPANYS");
+
+        CompanySearchItem expected = CompanySearchItem.Builder.builder()
+                .corporateNameStart("TEST COMPANY")
+                .corporateNameEnding("PLC")
+                .dateOfCreation(LocalDate.of(2000, 6, 24))
+                .fullAddress("")
+                .address(null)
+                .companyNumber("ABCD1234")
+                .externalRegistrationNumber("CDEF3456")
+                .dateOfCessation(LocalDate.of(2024, 6, 24))
+                .sicCodes(Arrays.asList("12345", "23456", "34567"))
+                .companyStatus("active")
+                .sameAsKey("TESTCOMPANY")
+                .wildcardKey("TESTCOMPANYS0")
+                .build();
+
+        // when
+        CompanySearchItem actual = converter.convert(model);
+
+        //then
+        assertEquals(expected, actual);
+        verify(companySearchAddressConverter).convert(
+                new RegisteredOfficeAddress(), CompanySearchAddress.class);
     }
 
     private RegisteredOfficeAddress getROAProperites() {
