28ec033f4f7b7a22b52c2463b26fdb4778106bbf
sthompsonCH <sthompson@companieshouse.gov.uk>
2025-04-30 13:56:53 +0100

    DSND-3272: Fix issue with encoded path
    * Previously, encoding the path manually result in an undesirable response from the alpha key service. E.g. %7 would be converted to PERCENTSEVEN. This should now be fixed by using a UriComponentsBuilder and the getForObject method on restTemplate.

--- src/test/java/uk/gov/companieshouse/search/api/service/AlphaKeyServiceTest.java
+++ src/test/java/uk/gov/companieshouse/search/api/service/AlphaKeyServiceTest.java
@@ -1,5 +1,15 @@
 package uk.gov.companieshouse.search.api.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.net.URI;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -7,19 +17,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.search.api.model.response.AlphaKeyResponse;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -35,7 +37,7 @@ class AlphaKeyServiceTest {
     private EnvironmentReader mockEnvironmentReader;
 
     private static final String SAME_AS_ALPHA_KEY = "sameAsAlphaKey";
-    private static final String ORDERED_ALPHA_KEY= "orderedAlphaKey";
+    private static final String ORDERED_ALPHA_KEY = "orderedAlphaKey";
     private static final String UPPERCASE_NAME = "upperCaseName";
     private static final String CORPORATE_NAME = "corporateName";
     private static final String URL = "url";
@@ -43,12 +45,13 @@ class AlphaKeyServiceTest {
     @Test
     @DisplayName("Test alpha key response returned successfully")
     void testAlphaKeyResponseSuccessful() {
-
-        ResponseEntity<AlphaKeyResponse> response = new ResponseEntity<>(createAlphaKeyResponse(), HttpStatus.OK);
-
         when(mockEnvironmentReader.getMandatoryString(anyString())).thenReturn(URL);
-
-        when(mockRestTemplate.getForEntity(anyString(), eq(AlphaKeyResponse.class))).thenReturn(response);
+        when(mockRestTemplate.getForObject(any(), eq(AlphaKeyResponse.class))).thenReturn(createAlphaKeyResponse());
+        URI uri = UriComponentsBuilder.fromUriString(URL)
+                .queryParam("name", CORPORATE_NAME)
+                .build()
+                .encode()
+                .toUri();
 
         AlphaKeyResponse alphaKeyResponse = alphaKeyService.getAlphaKeyForCorporateName(CORPORATE_NAME);
 
@@ -56,20 +59,23 @@ class AlphaKeyServiceTest {
         assertEquals(SAME_AS_ALPHA_KEY, alphaKeyResponse.getSameAsAlphaKey());
         assertEquals(ORDERED_ALPHA_KEY, alphaKeyResponse.getOrderedAlphaKey());
         assertEquals(UPPERCASE_NAME, alphaKeyResponse.getUpperCaseName());
+        verify(mockRestTemplate).getForObject(uri, AlphaKeyResponse.class);
     }
 
     @Test
     @DisplayName("Test alpha key response not returned due to rest client exception")
     void testNoResponseReturnedDueToException() {
-
         when(mockEnvironmentReader.getMandatoryString(anyString())).thenReturn(URL);
-
-        when(mockRestTemplate.getForEntity(anyString(), eq(AlphaKeyResponse.class)))
-            .thenThrow(RestClientException.class);
+        when(mockRestTemplate.getForObject(any(), eq(AlphaKeyResponse.class))).thenThrow(RestClientException.class);
+        URI uri = UriComponentsBuilder.fromUriString(URL)
+                .queryParam("name", CORPORATE_NAME)
+                .build()
+                .encode()
+                .toUri();
 
         AlphaKeyResponse alphaKeyResponse = alphaKeyService.getAlphaKeyForCorporateName(CORPORATE_NAME);
         assertNull(alphaKeyResponse);
-
+        verify(mockRestTemplate).getForObject(uri, AlphaKeyResponse.class);
     }
 
     private AlphaKeyResponse createAlphaKeyResponse() {
