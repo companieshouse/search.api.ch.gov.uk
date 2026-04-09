fbec4bfc446fab54c67d832b0098e81555b4c286
artemisgardnerch <agardner@companieshouse.gov,uk>
2026-04-09 14:19:03 +0100

    fixing interceptor path

--- src/main/java/uk/gov/companieshouse/search/api/SearchApiApplication.java
+++ src/main/java/uk/gov/companieshouse/search/api/SearchApiApplication.java
@@ -30,7 +30,7 @@ public class SearchApiApplication implements WebMvcConfigurer {
         registry.addInterceptor(loggingInterceptor).excludePathPatterns("/search/healthcheck");
         registry.addInterceptor(authorisationInterceptor).addPathPatterns(
                 "/advanced-search/companies/{company_number}",
-                "/advanced-search/companies/csv/{company_number}",
+                "/advanced-search/companies/csv",
                 "/alphabetical-search/companies/{company_number}",
                 "/officers-search/officers/{officer_id}",
                 "/disqualified-search/disqualified-officers/{officer_id}",
